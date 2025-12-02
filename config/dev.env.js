ac7a8e9e980558274ed4cb8131782024e01b6a58
Administrator <gaoyongqiang@xrateverse.com>
2025-12-02 14:15:48 +0800

    修改https

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,5 +5,5 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   BASE_API: '"http://localhost:8080"',
-  VUE_APP_FILE_BASE_URL: '"http://minio.ratev.ai/mall"'
+  VUE_APP_FILE_BASE_URL: '"https://minio.ratev.ai/mall"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"http://admin-api.ratev.ai"',
-  VUE_APP_FILE_BASE_URL: '"http://minio.ratev.ai/mall"'
+  BASE_API: '"https://admin-api.ratev.ai"',
+  VUE_APP_FILE_BASE_URL: '"https://minio.ratev.ai/mall"'
 }
