0e31109318ec9449b1120e017d26f2cc5242fc17
Administrator <gaoyongqiang@xrateverse.com>
2026-01-27 14:31:35 +0800

    更新 BASE_API 地址以修正 API 访问路径

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"https://admin-api.ratev.ai"',
+  BASE_API: '"https://admin.ratev.ai/api"',
   VUE_APP_FILE_BASE_URL: '"https://minio.ratev.ai/mall"'
 }
